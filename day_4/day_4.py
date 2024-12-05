5f664234d1c1f3f0b2c902f066c82ee00b8d70bc
Ramon Tanoeiro <ramon.tanoeiro@indebted.co>
2024-12-05 15:10:43 +1100

    refactored even further so the scan functions can check for the desired word/character

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -32,37 +32,54 @@ def scan_matrix_part1(word_matrix: list[list[str]]):
     return finds
 
 
-def scan_all_directions(word_matrix, line_index, col_index, length):
+def scan_matrix_part2(word_matrix: list[list[str]]):
+    """This function will scan the matrix data and depending on the position we're in it will look to all directions"""
+    matrix_size = len(word_matrix)
+    finds = 0
+    length = len("A")
+    for index_line, line in enumerate(word_matrix):
+        if index_line != matrix_size - 1:
+            # All lines have a line break by default, except the last one
+            line.remove("\n")
+        for index_column, _ in enumerate(line):
+            if word_matrix[index_line][index_column] == "A"
+
+                finds += scan_diagonally(word_matrix, index_line, index_column, length)
+    
+    return finds
+
+
+def scan_all_directions(word_matrix, line_index, col_index, length, desired):
     index_matches = 0
-    index_matches += scan_horizontally(word_matrix, line_index, col_index, length)
-    index_matches += scan_vertically(word_matrix, line_index, col_index, length)
-    index_matches += scan_diagonally(word_matrix, line_index, col_index, length)
+    index_matches += scan_horizontally(word_matrix, line_index, col_index, length, desired)
+    index_matches += scan_vertically(word_matrix, line_index, col_index, length, desired)
+    index_matches += scan_diagonally(word_matrix, line_index, col_index, length, desired)
     return index_matches
 
 
-def scan_horizontally(word_matrix, line_index, col_index, length):
+def scan_horizontally(word_matrix, line_index, col_index, length, desired):
     index_matches = 0
     word = forward_horizontal(word_matrix, line_index, col_index, length)
     reversed_word = backward_horizontal(word_matrix, line_index, col_index, length)
-    index_matches += validate_word(word, "XMAS")
-    index_matches += validate_word(reversed_word, "XMAS")
+    index_matches += validate_word(word, desired)
+    index_matches += validate_word(reversed_word,desired)
 
     return index_matches
 
 
-def scan_vertically(word_matrix, line_index, col_index, length):
+def scan_vertically(word_matrix, line_index, col_index, length, desired):
     index_matches = 0
 
     word = downward_vertical(word_matrix, line_index, col_index, length)
     reversed_word = upward_vertical(word_matrix, line_index, col_index, length)
 
-    index_matches += validate_word(word, "XMAS")
-    index_matches += validate_word(reversed_word, "XMAS")
+    index_matches += validate_word(word, desired)
+    index_matches += validate_word(reversed_word, desired)
 
     return index_matches
 
 
-def scan_diagonally(word_matrix, line_index, col_index, length):
+def scan_diagonally(word_matrix, line_index, col_index, length, desired):
     index_matches = 0
 
     forward_upward_word = forward_upward_diagonal(
@@ -78,10 +95,10 @@ def scan_diagonally(word_matrix, line_index, col_index, length):
         word_matrix, line_index, col_index, length
     )
 
-    index_matches += validate_word(forward_upward_word, "XMAS")
-    index_matches += validate_word(forward_downward_word, "XMAS")
-    index_matches += validate_word(backward_upward_word, "XMAS")
-    index_matches += validate_word(backward_downward_word, "XMAS")
+    index_matches += validate_word(forward_upward_word, desired)
+    index_matches += validate_word(forward_downward_word, desired)
+    index_matches += validate_word(backward_upward_word, desired)
+    index_matches += validate_word(backward_downward_word, desired)
 
     return index_matches
 
@@ -240,5 +257,6 @@ def backward_upward_diagonal(
 data = create_srting_from_input("day_4_input.txt")
 word_matrix = create_word_matrix(data)
 # word_matrix = word_matrix[:12]
-results_part_1 = scan_matrix_part1(word_matrix, "XMAS")
+results_part_1 = scan_matrix_part1(word_matrix)
+results_part_2 = scan_matrix_part2(word_matrix)
 print(f"The total number of XMAS findings is {results_part_1}")
